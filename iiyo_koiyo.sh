f77658ee9b81d0cc4d6cf0f8e9b5cf925df9c4fe
Masaya Takeda <844196@gmail.com>
2014-10-31 16:34:31 +0900

    add iiyo_koiyo.sh

---
+++ iiyo_koiyo.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+#   @(#) 擬似乱数を生成・表示し、引数にマッチするまでループします
+#
+#   Usage:
+#       iiyo_koiyo.sh [num]
+#       command | iiyo_koiyo.sh
+#
+#   Author:
+#       @84____ (Masaya Tk.)
+#
+
+base=${0##*/}
+
+_error() {
+    echo "${base%.sh}: invaild argument" 1>&2
+    exit 1
+}
+
+_randCreate() {
+    rand=$((${RANDOM} % 10))
+    printf ${rand}
+}
+
+if [ -p /dev/stdin ]; then
+    iiyo=$(cat -)
+elif [ -n "${1}" ]; then
+    iiyo=${1}
+else
+    _error
+fi
+
+if $(echo ${iiyo} | grep '[^0-9]' >/dev/null 2>&1); then
+    _error
+fi
+
+while :;
+do
+    _randCreate
+
+    i=${i}${rand}
+
+    if $(echo ${i} | grep "${iiyo}$" >/dev/null 2>&1); then
+        echo ''
+        break
+    fi
+
+    if [ ${#i} -eq ${#iiyo} ]; then
+        i=${i:1}
+    fi
+done
+
+exit 0
